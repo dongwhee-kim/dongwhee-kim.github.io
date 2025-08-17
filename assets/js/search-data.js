7d82638e24d58e579f9850cd60c507fa446bf22f
dongwhee-kim <62287136+dongwhee-kim@users.noreply.github.com>
2025-08-17 04:41:29 +0000

    Deploying to gh-pages from @ dongwhee-kim/dongwhee-kim.github.io@e4a7ee8f81ad615cf2f70b79e45a974f8cf9e85a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Teaching/";
           },
+        },{id: "nav-gallery",
+          title: "Gallery",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
